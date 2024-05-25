ec754d0e8f0236ce7f4915402c21d0a3b859e8c8
xnuke2 <xmnikit@gmail.com>
2024-05-25 16:20:23 +0300

    lab2

---
+++ lab2/Perceptron_main.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 20:24:56 2021
+
+@author: AM4
+"""
+import pandas as pd
+import numpy as np
+from neural import Perceptron
+
+
+df = pd.read_csv('data.csv')
+
+df = df.iloc[np.random.permutation(len(df))]
+y = df.iloc[0:100, 4].values
+y = np.where(y == "Iris-setosa", 1, -1)
+X = df.iloc[0:100, [0, 2]].values
+
+
+inputSize = X.shape[1] # количество входных сигналов равно количеству признаков задачи 
+hiddenSizes = 10 # задаем число нейронов скрытого (А) слоя 
+outputSize = 1 if len(y.shape) else y.shape[1] # количество выходных сигналов равно количеству классов задачи
+
+
+NN = Perceptron(inputSize, hiddenSizes, outputSize)
+
+NN.train(X, y, n_iter=5, eta = 0.01)
+
+y = df.iloc[:, 4].values
+y = np.where(y == "Iris-setosa", 1, -1)
+X = df.iloc[:, [0, 2]].values
+out, hidden_predict = NN.predict(X)
+
+sum(np.abs(out-y.reshape(-1, 1)))/2
